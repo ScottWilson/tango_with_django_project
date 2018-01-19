8c99755bb79e6ab4b296646332cf5976d9c50d10
Scott Wilson <2247638w@student.gla.ac.uk>
2018-01-19 12:56:02 +0000

    commit: end of section 4.3

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -17,6 +17,9 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 from rango import views
 
 urlpatterns = [
@@ -25,4 +28,6 @@ urlpatterns = [
     # above maps any URLs starting with
     # rango/ to be handled by the rango application
     url(r'^admin/', admin.site.urls),
+    # is '+' necessary??
+    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 ]
