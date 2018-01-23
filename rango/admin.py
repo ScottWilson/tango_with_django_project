a8e9178baf59d774b632522730034f5bcf5829ec
Scott Wilson <2247638w@student.gla.ac.uk>
2018-01-23 17:09:51 +0000

    commit: end of chapter 5

--- rango/admin.py
+++ rango/admin.py
@@ -3,5 +3,8 @@ from rango.models import Category, Page
 
 # Register your models here.
 
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('category', 'title', 'url')
+
 admin.site.register(Category)
-admin.site.register(Page)
+admin.site.register(Page, PageAdmin)
