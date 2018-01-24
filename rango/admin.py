aaaa666441845c8513827da98eb3694811bd45f3
Scott Wilson <2247638w@student.gla.ac.uk>
2018-01-24 15:46:54 +0000

    commit: end of section 6.3

--- rango/admin.py
+++ rango/admin.py
@@ -3,8 +3,11 @@ from rango.models import Category, Page
 
 # Register your models here.
 
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+
 class PageAdmin(admin.ModelAdmin):
     list_display = ('category', 'title', 'url')
 
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
--- rango/views.py
+++ rango/views.py
@@ -2,10 +2,43 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+# Import Category model
+from rango.models import Category
+from rango.models import Page
+
+def show_category(request, category_name_slug):
+    # Create context dictionary to be passed to
+    # template rendering engine
+    context_dict = {}
+
+    try:
+        # Try and find category name slug with the given name
+        # False: raises DoesNotExist exception
+        # True: returns one model instance
+        category = Category.objects.get(slug=category_name_slug)
+
+        # Retrieve all associated pages (either list of page
+        # objects or empty list)
+        pages = Page.objects.filter(category=category)
+
+        # Add results list to template context under name pages
+        context_dict['pages'] = pages
+        # Add category object from database
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        # Don't do anything - the following displays a 'no category'
+        # message
+
+        context_dict['category'] = None
+        context_dict['pages'] = None
+
+    return render(request, 'rango/category.html', context_dict)
+
 def index(request):
-    # Construct a dictionary to pass to the template engine as its context
-    # Note the key boldmessage is the same as {{ boldmessage }} in the template
-    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    # Query database for a list of all categories currently stored
+    # Order categories by number of likes in descending order (top 5 only)
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
 
     # Return a rendered response to send to the client
     # We make use of the shortcut function to make our lives easier
